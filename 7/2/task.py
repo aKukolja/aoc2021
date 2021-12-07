5b966d2d6567a6ba94c00d0980937ba6a333f24a
Antun Kukolja <antunkukolja@gmail.com>
2021-12-07 18:11:04 +0100

    mean

--- 7/2/task.py
+++ 7/2/task.py
@@ -10,6 +10,8 @@ class CrabArray:
 
     def __init__(self):
         self._crabs = {}
+        self._start = None
+        self._stop = None
 
     def add_crab(self, distance):
         crab_count = self._crabs.get(distance)
@@ -17,6 +19,14 @@ class CrabArray:
             self._crabs[distance] = 1
         else:
             self._crabs[distance] = crab_count + 1
+        if self._start is None:
+            self._start = distance
+        else:
+            self._start = min(distance, self._start)
+        if self._stop is None:
+            self._stop = distance
+        else:
+            self._stop = max(distance, self._stop)
 
     @staticmethod
     def _find_rms(crabs, position):
@@ -26,13 +36,20 @@ class CrabArray:
             rms = rms + fuel_spent
         return rms
 
-    def find_optimal_distance(self):
-        starting_distances = self._crabs.keys()
-        start = min(starting_distances)
-        stop = max(starting_distances)
-        all_solutions = {}
+    @staticmethod
+    def _crab_mean(crabs):
+        sum = 0
+        total_crabs = 0
+        for distance, count in crabs.items():
+            sum = sum + distance * count
+            total_crabs = total_crabs + count
+        return int(sum / total_crabs)
 
-        for position in range(start, stop + 1):
+    def find_optimal_distance(self):
+        mass = CrabArray._crab_mean(self._crabs)
+        possible = {mass - 1, mass, mass + 1}
+        all_solutions = {}
+        for position in possible:
             all_solutions[position] = CrabArray._find_rms(self._crabs, position)
 
         solution = min(all_solutions.items(), key=lambda x: x[1])
